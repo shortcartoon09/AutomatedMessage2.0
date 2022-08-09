10579ffe6dc3f1c16f116f76885379971f47d389
shortcartoon09 <shortcartoon09@gmail.com>
2022-08-09 16:25:35 +0600

    intal commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+import random
+
+name = input("Hello, what is your name? ")
+
+time.sleep(2)
+print("My name is Hello" + name)
+
+feeling = input("How are you today? ")
+
+time.sleep(2)
+if "good" in feeling:
+    print("I'm feeling good too!")
+else:
+    print("I'm sorry to hear that!")
+
+time.sleep(2)
+favcolour = input("What is your favourite colour? ")
+
+colours = ["Red","Green","Blue"]
+
+time.sleep(2)
+print("My favourite colour is " + random.choice(colours))
+
+time.sleep(2)
+print("wow")
